4ea6cc18aaacbe38556c47ec5623f9e1ea3fc508
NowickiLukasz <nowicki.lukasz93@gmail.com>
2022-08-10 21:30:57 +0000

    Added checkout succes logic

--- checkout/models.py
+++ checkout/models.py
@@ -7,8 +7,6 @@ from django.conf import settings
 from products.models import Product
 
 
-# Create your models here.
-
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
@@ -22,8 +20,8 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
-    order_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
-    grand_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False, default=0)
 
     def _generate_order_number(self):
         """Generate a rendom, unique order number using UUID"""
@@ -34,11 +32,11 @@ class Order(models.Model):
         Update Grand total each time a line item is added,
         accounting for the delivery costs
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.order_total = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
-            self.order_total = 0
+            self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
@@ -49,13 +47,13 @@ class Order(models.Model):
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
-        def __str__(self):
-            return self.order_number
+    def __str__(self):
+        return self.order_number
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False,  on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True,) # XS, S, M, L, XL
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
